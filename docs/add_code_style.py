151f96ba142977e52c6728bcfc6c80107c0f80d8
wyfcyx <wyfcyx@gmail.com>
2019-11-24 19:44:41 +0800

    rebuild

--- docs/add_code_style.py
+++ docs/add_code_style.py
@@ -1,7 +1,10 @@
 s = open('docs/gitbook/style.css').read()
-head = 'markdown-section code{'
-color_attribute = 'color:#bf616a;'
-s = s.replace(head, head + color_attribute)
+code = 'markdown-section code{'
+color_red = 'color:#bf616a;'
+code_in_pre = 'markdown-section pre>code{'
+color_inherit = 'color:inherit;'
+s = s.replace(code, code + color_red)
+s = s.replace(code_in_pre, code_in_pre + color_inherit)
 with open('docs/gitbook/style.css', 'w') as f:
     f.write(s)
 
---
+++ docs/plugin_examples.txt
@@ -0,0 +1,12 @@
+This text is {% em %}highlighted!{% endem %}
+
+> **[info] For info**
+>
+> This is a info!
+>
+
+23333
+
+<font color=red style="background:grey">*.json</font>
+
+$$a+b=c$$
