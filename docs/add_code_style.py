8f4b30015d3caa82e07a8c75f990b2f0828303cd
wyfcyx <wyfcyx@gmail.com>
2019-11-24 19:55:04 +0800

    compatible with chrome

--- docs/add_code_style.py
+++ docs/add_code_style.py
@@ -2,7 +2,7 @@ s = open('docs/gitbook/style.css').read()
 code = 'markdown-section code{'
 color_red = 'color:#bf616a;'
 code_in_pre = 'markdown-section pre>code{'
-color_inherit = 'color:inherit;'
+color_inherit = 'color:#ccc;'
 s = s.replace(code, code + color_red)
 s = s.replace(code_in_pre, code_in_pre + color_inherit)
 with open('docs/gitbook/style.css', 'w') as f:
