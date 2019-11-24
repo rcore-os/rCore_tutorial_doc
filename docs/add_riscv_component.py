3e97d3e290e008f92af5fc0b353af831fe2335ae
wyfcyx <wyfcyx@gmail.com>
2019-11-24 19:22:13 +0800

    clean

--- docs/add_riscv_component.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_path = 'node_modules/prismjs/components.json'
-
-data = json.load(open(json_path))
-data['languages']['riscv'] = {'title': 'RISC-V', 'owner': 'shinbokuow2'}
-with open(json_path, 'w') as f:
-    f.write(json.dumps(data, sort_keys = True, indent = 4))
--- docs/build.sh
+++
@@ -1,7 +0,0 @@
-gitbook install
-\cp prism-riscv.js node_modules/prismjs/components/
-python3 add_riscv_component.py
-rm -r docs/
-gitbook build
-mv _book/ docs/
-python3 add_code_style.py
