2d7ba00fcc1ab6540c3ea7e29017d5062ac47d56
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2020-02-11 18:40:38 +0800

    del html

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-# gitbook install
+gitbook install
 cp extensions/highlight/prism-riscv.js node_modules/prismjs/components/
 python3 extensions/highlight/add_riscv_component.py
-rm -r docs/
+rm -rf docs/
+git filter-branch --force --index-filter 'git rm --cached -r --ignore-unmatch docs' --prune-empty --tag-name-filter cat -- --all
 gitbook build
 mv _book/ docs/
 python3 extensions/highlight/add_code_style.py
+git add docs
+git commit -m "update html"
